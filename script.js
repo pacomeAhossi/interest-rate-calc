e8a325bcee05c68532b97fb7889096a43ebe5618
Pacome AHOSSI <pacomeahossi175@gmail.com>
2025-04-03 13:58:35 -0400

    Initial commit and implementing calculateSimpleInterest function

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const calculate = () => {
-  let p = document.getElementById("principle").value;
-  let r = document.getElementById("rate").value;
-  let t = document.getElementById("time").value;
-  let simpleInterest = (p * r * t) / 100;
-  let amount = p - simpleInterest;
+  let p = Number(document.getElementById("principal").value);
+  let r = Number(document.getElementById("rate").value);
+  let t = Number(document.getElementById("time").value);
+  let simpleInterest = calculateSimpleInterest(p, r, t);
+  let amount = calculateTotalPayableAmount(p, simpleInterest);
 
   let result = document.getElementById("result");
 
@@ -11,3 +11,11 @@ const calculate = () => {
   <div>Total Interest: <span>${simpleInterest.toFixed(2)}</span></div>
   <div>Total Amount: <span>${amount.toFixed(2)}</span></div>`;
 };
+
+const calculateSimpleInterest = (principal, rate, time) => {
+    return (principal * rate * time) / 100;
+}
+
+const calculateTotalPayableAmount = (principal, interestAmount) => {
+    return principal - interestAmount;
+}
